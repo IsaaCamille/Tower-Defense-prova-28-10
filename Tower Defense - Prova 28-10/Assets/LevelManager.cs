17841114a21aa64a059ad9901512c21c9aca2cf1
IsaaCamille <isabellyrosaoechsler@icloud.com>
2024-10-21 18:01:50 -0300

    Metodo que permite instanciar a classe LevelManager

--- Tower Defense - Prova 28-10/Assets/LevelManager.cs
+++ Tower Defense - Prova 28-10/Assets/LevelManager.cs
@@ -8,7 +8,11 @@ public class LevelManager : MonoBehaviour
     public Transform pontoInicial;//Representa o ponto inicial do caminho, ou seja, o local onde os inimigos começam a se mover
     public Transform[] caminho;//Um array de Transform que define o caminho que os inimigos devem seguir, com cada elemento representando um waypoint
 
+    private void Awake()
+    {
+        principal = this;// garantir que a instância do LevelManager seja atribuída à variável estática principal. Isso estabelece um ponto de acesso global para o LevelManager, permitindo que outras partes do jogo acessem seus dados (como o caminho dos inimigos) sem precisar criar novas instâncias ou passar referências manualmente
 
+    }
 
 
 }
