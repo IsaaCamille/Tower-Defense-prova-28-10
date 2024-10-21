7abdaa90fdc476c46d9463861018fa107d87dc94
IsaaCamille <isabellyrosaoechsler@icloud.com>
2024-10-21 18:52:37 -0300

    metodo principal da torre, ele permite que os inimigo LEVEM dano

--- Tower Defense - Prova 28-10/Assets/TorreGuerreira.cs
+++ Tower Defense - Prova 28-10/Assets/TorreGuerreira.cs
@@ -16,7 +16,15 @@ public class TorreGuerreira : ITorreDano
     public float Alcance => 2f; // A torre só pode atacar inimigos que estejam a uma distância de 2 ou menos
 
 
+    public void Atacar(IDamageble alvo) //Esse é o método principal da torre, que recebe como parâmetro um inimigo (alvo), que deve implementar uma interface IDamageble
+    {
+        if (alvo != null) //O método primeiro verifica se o alvo é válido (ou seja, se não é null)
+        {
+            Debug.Log($"{Nome} ataca o inimigo com {Dano} de dano."); //Se o alvo for válido, ele registra no console uma mensagem que a torre está atacando o inimigo com o valor de Dano
 
+            alvo.TakeDamage(Dano); // o método chama o TakeDamage(Dano)no alvo, o que aplica o dano ao inimigo
+        }
+    }
 
 
 }
