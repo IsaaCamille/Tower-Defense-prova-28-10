d260e7ab5486c0cbdda382f79ac5ea52f7b996aa
IsaaCamille <isabellyrosaoechsler@icloud.com>
2024-10-21 18:44:26 -0300

    metodo principal da torre, ele permite que as torres deem dano

--- Tower Defense - Prova 28-10/Assets/TorreArqueira.cs
+++ Tower Defense - Prova 28-10/Assets/TorreArqueira.cs
@@ -15,5 +15,15 @@ public class TorreArqueira : ITorreDano
 
     public float Alcance => 2; // define o alcance da torre como 2. Isso significa que a torre só pode atacar inimigos que estejam a uma distância de 2 ou menos
 
+    public void Atacar(IDamageble alvo) //Esse é o método principal da torre, que recebe como parâmetro um inimigo ( alvo), que deve implementar uma interface IDamageble
+    {
+        if (alvo != null) //O método primeiro verifica se o alvo é válido (ou seja, se não é null)
+        {
+            Debug.Log($"{Nome} ataca o inimigo com {Dano} de dano."); //Se o alvo for válido, ele registra no console (Debug.Log) uma mensagem que a torre está atacando o inimigo com o valor de Dano
+
+            alvo.TakeDamage(Dano); //Em seguida, o método chama o TakeDamage(Dano)no alvo, o que aplica o dano ao inimigo
+        }
+    }
+
 
 }
