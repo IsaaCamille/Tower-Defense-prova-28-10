db42f2b12718113e457fae8046148607192796a5
IsaaCamille <isabellyrosaoechsler@icloud.com>
2024-10-21 14:44:17 -0300

    vai verificar se o hit do collider é diferente de nulo

--- Tower Defense - Prova 28-10/Assets/BuilderTower.cs
+++ Tower Defense - Prova 28-10/Assets/BuilderTower.cs
@@ -29,6 +29,10 @@ public class BuilderTower : MonoBehaviour
             // Realiza o Raycast 2D
             RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
 
+            if (hit.collider != null)
+            {
+                Debug.Log("Raycast 2D bateu em: " + hit.collider.name);
+            }
         }
     }
 }
