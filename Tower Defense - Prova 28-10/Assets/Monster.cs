ee8b0d8f0fb92c3dab54fea2d63487f09e3a033d
IsaaCamille <isabellyrosaoechsler@icloud.com>
2024-10-21 18:04:50 -0300

    criação variavel valor inicial da vida do monstro

---
+++ Tower Defense - Prova 28-10/Assets/Monster.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour, IDamageble
+{
+    public int vidaAtual = 100;  // Valor inicial da vida do monstro
+
+
+  
+}
