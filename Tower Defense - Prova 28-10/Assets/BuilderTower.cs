c19e1bc3a52749df6a75bd3df5979d7f7ef6d856
IsaaCamille <isabellyrosaoechsler@icloud.com>
2024-10-21 14:26:01 -0300

    Criação das variaveis

---
+++ Tower Defense - Prova 28-10/Assets/BuilderTower.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuilderTower : MonoBehaviour
+{
+    public GameObject turretPrefab; // Prefab único da torre
+    public LayerMask plotLayerMask; // Máscara para detectar apenas os plots
+    private ITorreDano selectedTowerType; // Tipo da torre selecionado pelo botão
+    private GameObject selectedTurretPrefab; // Prefab da torre selecionado pelo botão
+
+
+
+
+}
