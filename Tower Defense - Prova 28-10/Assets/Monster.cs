97a9aa291da1e7eefe316d9bea62cf2bd6b5b415
IsaaCamille <isabellyrosaoechsler@icloud.com>
2024-10-22 13:49:14 -0300

    metodo para diminuir a vida do inimigo quando ele levar dano ( e fazer ele morrer)

--- Tower Defense - Prova 28-10/Assets/Monster.cs
+++ Tower Defense - Prova 28-10/Assets/Monster.cs
@@ -6,6 +6,18 @@ public class Monster : MonoBehaviour, IDamageble
 {
     public int vidaAtual = 100;  // Valor inicial da vida do monstro
 
+    // Implementa o método da interface IDamageble
+    public void TakeDamage(int dano) //A função desse método é subtrair o valor de dano da vida
+    {
+        vidaAtual -= dano; // a vida do inimigo é reduzida pela quantidade de dano recebido
 
-  
+        Debug.Log("Inimigo tomou dano: " + dano + ", Vida restante: " + vidaAtual); //a quantidade de dano que o inimigo acabou de receber(dano)
+                                                                                    //a quantidade de vida restante do inimigo(vidaAtual)
+
+        if (vidaAtual <= 0) //O método verifica se a vida atual do inimigo(vidaAtual) é menor ou igual a 0.Se for,significa que o inimigo não tem mais vida suficiente para continuar no jogo
+        {
+            Morrer();  // Chama o método Morrer quando a vida chegar a zero
+        }
+    }
+
 }
