457b36c6315ee9c7a0b4dc697c872480080d417e
IsaaCamille <isabellyrosaoechsler@icloud.com>
2024-11-25 20:10:07 -0300

    criação das variaveis e inicialização do banner

--- Tower Defense - Prova 28-10/Assets/Code/Scripts/ADSManager.cs
+++ Tower Defense - Prova 28-10/Assets/Code/Scripts/ADSManager.cs
@@ -5,9 +5,20 @@ using UnityEngine.Advertisements;
 
 public class ADSManager : MonoBehaviour, IUnityAdsInitializationListener, IUnityAdsLoadListener, IUnityAdsShowListener
 {
+    private string bannerPlacementId = "Banner_Android";
 
+    private void Awake()
+    {
+        Advertisement.Initialize("5730091", true, this);
 
+    }
 
+    public void LoadBanner()
+    {
+        Advertisement.Banner.SetPosition(BannerPosition.TOP_CENTER);
+        Advertisement.Banner.Show(bannerPlacementId);
+        Debug.Log("Banner initialized sussefully");
+    }
 
 
 
