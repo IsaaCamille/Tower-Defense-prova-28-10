1a9c77743c240a69436b1a43b7cf662715bdb827
IsaaCamille <isabellyrosaoechsler@icloud.com>
2024-11-25 19:58:36 -0300

    vou fazer o banner td de novo

--- Tower Defense - Prova 28-10/Assets/Code/Scripts/ADSManager.cs
+++ Tower Defense - Prova 28-10/Assets/Code/Scripts/ADSManager.cs
@@ -6,62 +6,12 @@ using UnityEngine.Advertisements;
 public class ADSManager : MonoBehaviour, IUnityAdsInitializationListener, IUnityAdsLoadListener, IUnityAdsShowListener
 {
 
-    private string bannerPlacementId = "Banner_Android";
-
-    private void Awake()
-    {
-        Advertisement.Initialize("5730091", true, this);
-    }
-    private void LoadBanner()
-    {
-        Advertisement.Banner.SetPosition(BannerPosition.TOP_CENTER);
-        Advertisement.Banner.Show(bannerPlacementId);
-        Debug.Log("Banner initialized sussefully");
-    }
 
 
-    // Callbacks da inicialização
-    public void OnInitializationComplete()
-    {
-        Debug.Log("Unity Ads initialized successfully.");
-        LoadBanner();
-    }
 
-    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
-    {
-        Debug.LogError($"Unity Ads initialization failed: {error} - {message}");
-    }
 
-    // Callbacks da exibição de anúncios
-    public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
-    {
-      
-    }
 
-    public void OnUnityAdsAdLoaded(string placementId)
-    {
-        Debug.Log($"Ad Loaded: {placementId}");
-    }
 
-    public void OnUnityAdsFailedToLoad(string placementId, UnityAdsLoadError error, string message)
-    {
-        Debug.LogError($"Failed to load ad {placementId}: {error} - {message}");
-    }
-
-    public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnUnityAdsShowStart(string placementId)
-    {
-        
-    }
-
-    public void OnUnityAdsShowClick(string placementId)
-    {
-        throw new System.NotImplementedException();
-    }
 }
 
 
