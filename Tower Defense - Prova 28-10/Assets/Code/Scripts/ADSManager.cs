624a63386fddbc1df45599f57d7444bb7d7cb753
IsaaCamille <isabellyrosaoechsler@icloud.com>
2024-11-26 13:52:14 -0300

    Squashed commit of the following:
    commit 9d549aabc31ff4a5fbb2f11c942fe15f9fc1cfe8
    Author: IsaaCamille <isabellyrosaoechsler@icloud.com>
    Date:   Mon Nov 25 21:01:31 2024 -0300
    
        configurando tela game over
    
    commit e2fc9275e158ede1c479f21cd2c83a299211f66d
    Author: IsaaCamille <isabellyrosaoechsler@icloud.com>
    Date:   Mon Nov 25 20:38:44 2024 -0300
    
        declarando showAd
    
    commit 793e7a78648c689cd022e093c3ffce516cf9749d
    Author: IsaaCamille <isabellyrosaoechsler@icloud.com>
    Date:   Mon Nov 25 20:32:06 2024 -0300
    
        declarando callback
    
    commit 041bd87478161a75b7b81fc32b96abcad1a34bb0
    Author: IsaaCamille <isabellyrosaoechsler@icloud.com>
    Date:   Mon Nov 25 20:28:43 2024 -0300
    
        metodo resume game
    
    commit 9b2192b956b7150b2071af29685743fa7a08f6c6
    Author: IsaaCamille <isabellyrosaoechsler@icloud.com>
    Date:   Mon Nov 25 20:27:31 2024 -0300
    
        metodo pause game
    
    commit 085f5ceb9cafc402dbb5977d1f235d71b9ed0ca6
    Author: IsaaCamille <isabellyrosaoechsler@icloud.com>
    Date:   Mon Nov 25 20:26:10 2024 -0300
    
        declrando recompensado
    
    commit afadc0d076c77f2fce983474a7d26ce08625be25
    Author: IsaaCamille <isabellyrosaoechsler@icloud.com>
    Date:   Mon Nov 25 20:24:17 2024 -0300
    
        declarando interstitial nao pulavel
    
    commit 1b587291163c58e8e79371227a9e149817bd7839
    Author: IsaaCamille <isabellyrosaoechsler@icloud.com>
    Date:   Mon Nov 25 20:20:44 2024 -0300
    
        declarando interstitial
    
    commit c05d63f5974dbfc60ed477857b1299f0f747b3f8
    Author: IsaaCamille <isabellyrosaoechsler@icloud.com>
    Date:   Mon Nov 25 20:15:00 2024 -0300
    
        banner feito (só falta os callback)
    
    commit f891fb769c95a0b6a8c9f8751c1a6bf4b50de619
    Author: IsaaCamille <isabellyrosaoechsler@icloud.com>
    Date:   Mon Nov 25 20:11:49 2024 -0300
    
        ciclo do banner (fazer ele aparecer e sumir)
    
    commit 457b36c6315ee9c7a0b4dc697c872480080d417e
    Author: IsaaCamille <isabellyrosaoechsler@icloud.com>
    Date:   Mon Nov 25 20:10:07 2024 -0300
    
        criação das variaveis e inicialização do banner
    
    commit 1a9c77743c240a69436b1a43b7cf662715bdb827
    Author: IsaaCamille <isabellyrosaoechsler@icloud.com>
    Date:   Mon Nov 25 19:58:36 2024 -0300
    
        vou fazer o banner td de novo
    
    commit b1fa508df92ed4e29640c6a2ed1e0df6052bbd0a
    Author: IsaaCamille <isabellyrosaoechsler@icloud.com>
    Date:   Sat Nov 23 13:14:44 2024 -0300
    
        Banner está dando erro (vou arrumar)
    
    commit 76f75b013ac4d421884bbcbd05f8042af137929e
    Author: IsaaCamille <isabellyrosaoechsler@icloud.com>
    Date:   Sat Nov 23 12:49:43 2024 -0300
    
        Anuncio banner feito
    
    commit a21ac588d754d4fcae3c24900e03fcd4f2f58a41
    Author: IsaaCamille <isabellyrosaoechsler@icloud.com>
    Date:   Sat Nov 23 12:35:40 2024 -0300
    
        Painel de Game Over feito
    
    commit 613fac4f486ca3f8cdf35c3a458a7b05c8198ca0
    Author: IsaaCamille <isabellyrosaoechsler@icloud.com>
    Date:   Sat Nov 23 11:31:45 2024 -0300
    
        Criação do script ADSManager

--- Tower Defense - Prova 28-10/Assets/Code/Scripts/ADSManager.cs
+++ Tower Defense - Prova 28-10/Assets/Code/Scripts/ADSManager.cs
@@ -3,28 +3,140 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Advertisements;
 
-public class ADSManager : MonoBehaviour
-{ 
-    
+public class ADSManager : MonoBehaviour, IUnityAdsInitializationListener, IUnityAdsLoadListener, IUnityAdsShowListener
+{
+    private string bannerPlacementId = "Banner_Android";
+    private string interstitialPlacementId = "Interstitial_Android";
+    private string nonSkkipableInterstitial = "Interstitial_Nao_Pulavel";
+    private string rewardedPlacementId = "Rewarded_Android";
 
+    private bool showSkippable = true;
 
+    public delegate void ShowAds();
 
+    public ShowAds showAds;
 
+    private void Awake()
+    {
+        Advertisement.Initialize("5730091", true, this);
 
+    }
 
+    public void LoadBanner()
+    {
+        Advertisement.Banner.SetPosition(BannerPosition.TOP_CENTER);
+        Advertisement.Banner.Show(bannerPlacementId);
+        Debug.Log("Banner initialized sussefully");
+        StartCoroutine(BannerCycleCoroutine());
+    }
 
+    private IEnumerator BannerCycleCoroutine()
+    {
+        while (true) // Ciclo infinito
+        {
+            Advertisement.Banner.Show(bannerPlacementId); // exibe o banner
+            Debug.Log("Banner exibido.");
+            yield return new WaitForSeconds(10f); // mantém o banner visível por 10 segundos
 
+            Advertisement.Banner.Hide(); // oculta o banner
+            Debug.Log("Banner ocultado.");
+            yield return new WaitForSeconds(5f); // mantém o banner oculto por 5 segundos
+        }
+    }
 
+    public void ShowInterstitial()
+    {
+        PauseGame();
+        Advertisement.Show(interstitialPlacementId, this);
+        Debug.Log("Interstistial initialized sussefully");
 
+    }
 
+    public void ShowNonSkippable()
+    {
+        PauseGame();
+        Advertisement.Show(nonSkkipableInterstitial, this);
+        Debug.Log("Non Skkipable initialized sussefully");
+    }
 
+    public void ShowRewarded()
+    {
+        PauseGame();
+        Advertisement.Show(rewardedPlacementId, this);
+        Debug.Log("Rewarded initialized sussefully");
+    }
 
+    public void ShowAd()
+    {
+        if (showSkippable)
+        {
+            ShowInterstitial();
+        }
+        else
+        {
+            ShowNonSkippable();
+        }
 
+        // Alterna o tipo de anúncio para a próxima vez
+        showSkippable = !showSkippable;
+    }
 
+    private void PauseGame()
+    {
+        Time.timeScale = 0f;
+        Debug.Log("Game Paused for Ad.");
+    }
+
+    private void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        Debug.Log("Game Resumed after Ad.");
+    }
+
+    public void OnInitializationComplete()
+    {
+        Debug.Log("Unity Ads initialized successfully.");
+        showAds += ShowRewarded;
+        LoadBanner();
+    }
+
+    public void OnUnityAdsShowComplete(string placementId, UnityAdsShowCompletionState showCompletionState)
+    {
+        ResumeGame();
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        Debug.LogError($"Unity Ads initialization failed: {error} - {message}");
+    }
+
+    public void OnUnityAdsAdLoaded(string placementId)
+    {
+        Debug.Log($"Ad Loaded: {placementId}");
+    }
+
+    public void OnUnityAdsFailedToLoad(string placementId, UnityAdsLoadError error, string message)
+    {
+        Debug.LogError($"Failed to load ad {placementId}: {error} - {message}");
+    }
+
+    public void OnUnityAdsShowFailure(string placementId, UnityAdsShowError error, string message)
+    {
+
+    }
+
+    public void OnUnityAdsShowStart(string placementId)
+    {
+
+    }
+
+    public void OnUnityAdsShowClick(string placementId)
+    { }
 
 
 
 }
+
 
 
 
